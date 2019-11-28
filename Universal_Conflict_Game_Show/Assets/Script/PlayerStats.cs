ce7ab460c747c1ce8dcbe0d78c56a4ea5ce56c47
jhmauritz <46462219+jhmauritz@users.noreply.github.com>
2019-11-28 11:33:24 -0600

    Moved bullet to parent script

--- Universal_Conflict_Game_Show/Assets/Script/PlayerStats.cs
+++ Universal_Conflict_Game_Show/Assets/Script/PlayerStats.cs
@@ -5,11 +5,6 @@ using UnityEngine.UI;
 
 public class PlayerStats : Stats
 {
-    public GameObject bulletEmitter;
-    public GameObject bullet;
-    public float bulletSpeed;
-    GameObject TempBullHandle;
-
     public override void Awake()
     {
         base.Awake();
@@ -21,22 +16,7 @@ public class PlayerStats : Stats
 
         if(Input.GetKeyDown(KeyCode.E))
         {
-            //Bullet Initiation
-            TempBullHandle = Instantiate(bullet, bulletEmitter.transform.position, bulletEmitter.transform.rotation) as GameObject;
-            TempBullHandle.SetActive(true);
-
-            //Correct Bullet Rotation
-            TempBullHandle.transform.Rotate(Vector3.left * 90);
-
-            //Retrieve and Control Bullet RigidBody
-            Rigidbody TempRigBod;
-            TempRigBod = TempBullHandle.GetComponent<Rigidbody>();
-
-            //Move Bullet Forward
-            TempRigBod.AddForce(transform.forward * bulletSpeed);
-
-            //Clean Up, Bullet Destroy 
-            Destroy(TempBullHandle, 3f);
+            BulletSpawm();
         }
     }
 
