efcf60dd2b545e6b32a61264e6e7039fc37088ab
jhmauritz <jesse.mauritz.1>
2019-11-29 10:28:23 -0600

    Changes made

---
+++ Universal_Conflict_Game_Show/Assets/Script/CameraAndPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraAndPlayer : MonoBehaviour
+{
+    public Shader baseShader;
+    public Shader fadeShader;
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Player has entered the trigger");
+
+        if (other.gameObject.GetComponent<PlayerNavPoint>())
+        {
+            GetComponent<Renderer>().material.shader = fadeShader;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Player has exited the trigger");
+
+        if (other.gameObject.GetComponent<PlayerNavPoint>())
+        {
+            GetComponent<Renderer>().material.shader = baseShader;
+        }
+    }
+}
